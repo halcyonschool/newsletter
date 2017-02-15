566f36bf78f0396ad4034aaa834eab30afe35a7f
Andrew Howden <andrew.howden@north27.co.uk>
2017-02-15 14:03:42 +0000

    add signage v1

---
+++ signage/weather/weather.js
@@ -0,0 +1,26 @@
+// v3.1.0
+//Docs at http://simpleweatherjs.com
+
+$(document).ready(function() {  
+  getWeather(); //Get the initial weather.
+  setInterval(getWeather, 600000); //Update the weather every 10 minutes.
+});
+
+function getWeather() {
+  $.simpleWeather({
+    location: 'London, England',
+    woeid: '',
+    unit: 'c',
+    success: function(weather) {
+      html = '<h2><i class="icon-'+weather.code+'"></i><p> '+weather.temp+'&deg;'+weather.units.temp+'</h2>';
+      html += '<br><br>'+weather.city+', '+weather.region+'';
+      html += '<br><br>'+weather.currently+'';
+      html += '<br><br>'+weather.wind.direction+' '+weather.wind.speed+' '+weather.units.speed+'';
+  
+      $("#weather").html(html);
+    },
+    error: function(error) {
+      $("#weather").html('<p>'+error+'</p>');
+    }
+  });
+};
